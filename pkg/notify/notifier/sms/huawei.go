d1b1e0dc865d048199be0b958fbec82b6e3959a1
zhu733756 <talonzhu@yunify.com>
2021-07-28 15:29:44 +0800

    fix SMS provider huawei
    Signed-off-by: zhu733756 <talonzhu@yunify.com>

--- pkg/notify/notifier/sms/huawei.go
+++ pkg/notify/notifier/sms/huawei.go
@@ -8,8 +8,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	"github.com/kubesphere/notification-manager/pkg/apis/v2beta2"
 	"github.com/kubesphere/notification-manager/pkg/notify/config"
@@ -42,15 +44,14 @@ func NewHuaweiProvider(c *config.Config, providers *v2beta2.Providers, phoneNumb
 	phoneNum := handleHuaweiPhoneNums(phoneNumbers)
 	u := handleHuaweiUrl(providers.Huawei.Url)
 	return &HuaweiNotifier{
-		Signature:     providers.Huawei.Signature,
-		NotifierCfg:   c,
-		TemplateId:    providers.Huawei.TemplateId,
-		AppKey:        providers.Huawei.AppKey,
-		AppSecret:     providers.Huawei.AppSecret,
-		PhoneNums:     phoneNum,
-		Sender:        providers.Huawei.Sender,
-		TemplateParas: providers.Huawei.TemplateParas,
-		Url:           u,
+		Signature:   providers.Huawei.Signature,
+		NotifierCfg: c,
+		TemplateId:  providers.Huawei.TemplateId,
+		AppKey:      providers.Huawei.AppKey,
+		AppSecret:   providers.Huawei.AppSecret,
+		PhoneNums:   phoneNum,
+		Sender:      providers.Huawei.Sender,
+		Url:         u,
 	}
 }
 
@@ -61,15 +62,16 @@ func (h *HuaweiNotifier) MakeRequest(ctx context.Context, messages string) error
 	}
 	appSecret, err := h.NotifierCfg.GetCredential(h.AppSecret)
 	if err != nil {
-		return fmt.Errorf("[Huawei SendSms] cannot get appSecret: %s", err.Error())
+		return fmt.Errorf("[Huawei SendSms]cannot get appSecret: %s", err.Error())
 	}
 
+	m := strings.Join(generateParams(messages), ",")
 	// body
 	body := strings.NewReader(url.Values{
 		"from":           {h.Sender},
 		"to":             {h.PhoneNums},
 		"templateId":     {h.TemplateId},
-		"templateParas":  {`[" + Messages + "]`},
+		"templateParas":  {"[" + m + "]"},
 		"signature":      {h.Signature},
 		"statusCallback": {""},
 	}.Encode())
@@ -104,18 +106,51 @@ func (h *HuaweiNotifier) MakeRequest(ctx context.Context, messages string) error
 		return fmt.Errorf("[Huawei  SendSms] unmarshal failed: %s", fmt.Errorf(err.Error()))
 	}
 	if r.Code != "000000" {
-		return fmt.Errorf("[Huawei  SendSms] send failed: %s, code: %s", r.Description, r.Code)
+		return fmt.Errorf("[Huawei  SendSms] send failed: %s, code: %s, raw messages: %s, handled messages: %s", r.Description, r.Code, messages, m)
 	}
 	return nil
-
 }
 
 func buildWSSEHeader(appKey string, appSecret string) string {
 	now := time.Now().Format("2006-01-02T15:04:05Z")
 	nonce := strings.ReplaceAll(string(uuid.NewUUID()), "-", "")
-	sha256 := getSha256Code(nonce + now + appSecret)
+	sha256 := getSha256Code(nonce + now + strings.ReplaceAll(appSecret, "\n", ""))
 	passwordDigest := base64.StdEncoding.EncodeToString([]byte(sha256))
-	return fmt.Sprintf(`"UsernameToken Username="%s",PasswordDigest="%s",Nonce="%s",Created="%s"`, appKey, passwordDigest, nonce, now)
+	return fmt.Sprintf(`UsernameToken Username="%s",PasswordDigest="%s",Nonce="%s",Created="%s"`, strings.ReplaceAll(appKey, "\n", ""), passwordDigest, nonce, now)
+}
+
+// Note here, the keys corresponding to the return values are alertname,alerttype,severity,message,summary.
+// Pls make your custom SMS template containing five placeholders, you can create an SMS template in Huawei Cloud's SMS console.
+// i.e: Reviced notifications: alertname:${TEXT}, alerttype:${TEXT}, serverity:${TEXT}, message:${TEXT}, summary:${TEXT}
+func generateParams(messages string) []string {
+	messagePat := `alertname=(?P<alertname>.*?)\s+(?s).*alerttype\s?=\s?(?P<alerttype>[a-zA-Z]+)\s+(?s).*severity\s?=\s?(?P<severity>[a-zA-Z]+)\s+(?s).*message\s?=\s?(?P<message>.*?)\s+(?s).*summary\s?=\s?(?P<summary>.*[^\"])`
+	re := regexp.MustCompile(messagePat)
+
+	matches := re.FindStringSubmatch(messages)
+	sa := make([]string, 0)
+	if len(matches) == 0 {
+		// Keep the length of the string less than 20 characters.
+		if utf8.RuneCountInString(messages) > 20 {
+			messages = string([]rune(messages)[:20])
+		}
+		sa = append(sa, messages)
+		return sa
+	}
+
+	stripedRe := regexp.MustCompile(`\s+`)
+
+	for i, m := range matches {
+		if i > 0 {
+			mn := stripedRe.ReplaceAllString(m, "")
+			// Keep the length of the string less than 20 characters.
+			if utf8.RuneCountInString(mn) > 20 {
+				mn = string([]rune(mn)[:20])
+			}
+			sa = append(sa, `"`+mn+`"`)
+		}
+	}
+
+	return sa
 }
 
 func handleHuaweiUrl(u string) string {
@@ -126,8 +161,8 @@ func handleHuaweiUrl(u string) string {
 }
 
 func handleHuaweiPhoneNums(phoneNumbers []string) string {
-	if len(phoneNumbers) > aliyunMaxPhoneNums {
-		phoneNumbers = phoneNumbers[:aliyunMaxPhoneNums]
+	if len(phoneNumbers) > HuaweiMaxPhoneNums {
+		phoneNumbers = phoneNumbers[:HuaweiMaxPhoneNums]
 	}
 	huaweiPhoneNums := make([]string, 0)
 	for _, p := range phoneNumbers {
