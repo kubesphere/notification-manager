1c82cf4423932d7d417c9e3e22901d0c66fc3643
ctrought <65360454+ctrought@users.noreply.github.com>
2022-05-27 00:23:07 -0400

    update nm rbac markers

--- controllers/notificationmanager_controller.go
+++ controllers/notificationmanager_controller.go
@@ -66,7 +66,7 @@ type NotificationManagerReconciler struct {
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
-// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=get;list;watch;
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch
 
 func (r *NotificationManagerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 
