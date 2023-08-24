0a25f54358d31e2de8f88dde39e1fff1f414e582
wanjunlei <53003665+wanjunlei@users.noreply.github.com>
2023-08-24 18:11:13 +0800

    add a new html subject template (#220)
    * add a new html subject template
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    * fix ci error
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    * fix ci error
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    * fix ci error
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    * fix ci error
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    * add missing role
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    * add missing role
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    * add missing role
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>
    
    ---------
    
    Signed-off-by: wanjunlei <wanjunlei@kubesphere.io>

--- controllers/notificationmanager_controller.go
+++ controllers/notificationmanager_controller.go
@@ -67,6 +67,7 @@ type NotificationManagerReconciler struct {
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 // +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
 
 func (r *NotificationManagerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 
